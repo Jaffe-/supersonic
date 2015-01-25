769aa5fe693a394f21ab6a30d9ada393be84fa06
Jaffe- <jaffe1@gmail.com>
2015-01-25 16:49:49 +0000

    clock.c/h -> pulse.c/h, la til displayfiler

---
+++ display.c
@@ -0,0 +1,17 @@
+/* 
+   Displayfunksjoner
+
+   Display er koblet til følgende pinner:
+
+   Display-pinne    Signal      Atmega
+   4                RS          PD0
+   6                E           PB0
+   7 - 11           DB0 - DB4   PD0 - PD4
+   12, 13           DB5, DB6    PB6, PB7
+   14               DB7         PD5
+*/
+
+#include "display.h"
+#include <avr/io.h>
+
+/* kode ... */
