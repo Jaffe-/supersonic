552ac1513a519536518f3114bfa774b00e56fe21
Jaffe- <jaffe1@gmail.com>
2015-02-10 23:51:02 +0000

    fix

--- main.c
+++ main.c
@@ -9,211 +9,27 @@
 #include "input.h"
 #include "animation.h"
 
-
-inline float fsquare(float f)
-{
-  return f * f;
-}
-
 int main()
 {
   //while(1);
   display_setup(); 
   input_setup();
   pulse_setup();
-
-  display_setcursor(1,1,7);
-  display_print_string("HEI");
-  while(1);
-  
-  uint8_t bupp_frames[][8] = {
-    {0b01110,
-     0b10001,
-     0b00000,
-     0b00000,
-     0b01110,
-     0b10001,
-     0b00000,
-     0b00100},
     
-    {0b10001,
-     0b00000,
-     0b00000,
-     0b01110,
-     0b10001,
-     0b00000,
-     0b00100,
-     0b01010},
-
-    {0b00000,
-     0b00000,
-     0b01110,
-     0b10001,
-     0b00000,
-     0b01110,
-     0b10001,
-     0b00000}
-  };
-
-  uint16_t logo_frames[][16] = {
-    {
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000100000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000
-    },
-    {
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000100000,
-      0b0001010000,
-      0b0000100000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000
-    },
-    {
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0001100000,
-      0b0010010000,
-      0b0100001000,
-      0b0010010000,
-      0b0001100000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000
-    },
-    {
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0011100000,
-      0b0100010000,
-      0b0100001000,
-      0b1000001000,
-      0b1000001000,
-      0b0100010000,
-      0b0011100000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000
-    },
-    {
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0111110000,
-      0b1000001000,
-      0b0000000100,
-      0b0000000100,
-      0b0000000100,
-      0b0000000100,
-      0b1000000100,
-      0b0110011000,
-      0b0001100000,
-      0b0000000000,
-      0b0000000000,
-      0b0000000000
-    },
-    {
-      0b0000000000,
-      0b0000000000,
-      0b0000000000,
-      0b0111110000,
-      0b1000001000,
-      0b0000000100,
-      0b0000000010,
-      0b0000000010,
-      0b0000000010,
-      0b0000000010,
-      0b0000000010,
-      0b1000000100,
-      0b0100001000,
-      0b0011110000,
-      0b0000000000,
-      0b0000000000
-    }
-  };
-
-  
-  animation_t logo = {.width = 2, .height = 2,
-		      .frames = logo_frames,
-		      .num_frames = 6,
-		      .first_character = 1,
-		      .count = 2};
-  /*
-  animation_t bupp = {.width = 1, .height = 1,
-		      .frames = bupp_frames,
-		      .num_frames = 3,
-		      .first_character = 0,
-		      .count = 8};
-*/    
-  display_setcursor(1, 2, 1);
-  display_print_string("Fuck you! ");
-
-
-  //animation_init(&logo);
-  //animation_display(&logo, 1, 1, 8);
-  
-  //  display_setcursor(1, 1, 1);
-  // display_print_float(distance);
-  display_setcursor(2, 1, 1);
-  display_print_string("Fuck you too!AAAAAA");
-  
   while(1) {
-    animation_animate(&logo);
     input_update();
-    //display_clear();
 
     if (buffer_unread_elements() >= 8) {
       float distance_sum = 0;
-      float distance_min = 1000;
       for (uint8_t i = 0; i < 8; i++) {
 	float dist = (buffer_read() * 340.0) / (2 * 1000000.0);
 	distance_sum += dist;
-	if (dist < distance_min)
-	  distance_min = dist;
       }
       float mean = distance_sum / 8.0;
-      float distance = sqrtf(fsquare(mean) - fsquare(0.0325));
 
-      //float distance = distance_sum / 4.0;
       display_clear();
       display_setcursor(1,1,1);
-      display_print_float(distance * 100);
-      display_setcursor(1,2,1);
-      display_print_float(distance_min * 100);
+      display_print_float(mean * 100);
     }
 
     if (input_pressed(BUTTON_RIGHT))
