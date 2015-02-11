7b301f3c20a940a7e0377455214b7ce9f919c940
Jaffe- <jaffe1@gmail.com>
2015-02-11 19:47:07 +0000

    ny schematic

--- main.c
+++ main.c
@@ -17,25 +17,26 @@ int main()
   pulse_setup();
     
   while(1) {
-    input_update();
+    //input_update();
 
-    if (buffer_unread_elements() >= 8) {
+    if (buffer_unread_elements() >= 4) {
       float distance_sum = 0;
-      for (uint8_t i = 0; i < 8; i++) {
+      for (uint8_t i = 0; i < 4; i++) {
 	float dist = (buffer_read() * 340.0) / (2 * 1000000.0);
 	distance_sum += dist;
       }
-      float mean = distance_sum / 8.0;
+      float mean = distance_sum / 4.0;
 
       display_clear();
       display_setcursor(1,1,1);
-      display_print_float(mean * 100);
+      display_print_integer(mean * 100);
     }
 
+    /*
     if (input_pressed(BUTTON_RIGHT))
       display_move_window(2);
     if (input_pressed(BUTTON_LEFT))
       display_move_window(1);
-      
+    */
   }
 }
